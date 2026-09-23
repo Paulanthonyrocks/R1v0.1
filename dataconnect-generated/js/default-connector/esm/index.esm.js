41a26456b4d31171dd3f7b709e96c8992017ccca
lewisjeffrey726 <lewisjeffrey726@gmail.com>
2026-09-23 01:27:13 +0000

    gg

--- dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,7 +0,0 @@
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'r1v01',
-  location: 'us-central1'
-};
-
--- dataconnect-generated/js/default-connector/index.cjs.js
+++
@@ -1,7 +0,0 @@
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'r1v01',
-  location: 'us-central1'
-};
-exports.connectorConfig = connectorConfig;
